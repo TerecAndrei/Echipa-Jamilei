2d28035fe81920b7aaf86637c5129e66d20db097
Bianca <bianca.terciu@gmail.com>
2023-03-10 14:49:29 +0200

    sonarLint issues fixed

--- src/main/java/pizzashop/repository/MenuRepository.java
+++ src/main/java/pizzashop/repository/MenuRepository.java
@@ -11,22 +11,19 @@ public class MenuRepository {
     private static String filename = "data/menu.txt";
     private List<MenuDataModel> listMenu;
 
-    public MenuRepository(){
+    public MenuRepository() {
     }
 
-    private void readMenu(){
+    private void readMenu() {
         //ClassLoader classLoader = MenuRepository.class.getClassLoader();
         File file = new File(filename);
-        this.listMenu= new ArrayList();
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(file));
+        this.listMenu = new ArrayList();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line = null;
-            while((line=br.readLine())!=null){
-                MenuDataModel menuItem=getMenuItem(line);
+            while ((line = br.readLine()) != null) {
+                MenuDataModel menuItem = getMenuItem(line);
                 listMenu.add(menuItem);
             }
-            br.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -34,17 +31,17 @@ public class MenuRepository {
         }
     }
 
-    private MenuDataModel getMenuItem(String line){
-        MenuDataModel item=null;
-        if (line==null|| line.equals("")) return null;
-        StringTokenizer st=new StringTokenizer(line, ",");
-        String name= st.nextToken();
+    private MenuDataModel getMenuItem(String line) {
+        MenuDataModel item = null;
+        if (line == null || line.equals("")) return null;
+        StringTokenizer st = new StringTokenizer(line, ",");
+        String name = st.nextToken();
         double price = Double.parseDouble(st.nextToken());
         item = new MenuDataModel(name, 0, price);
         return item;
     }
 
-    public List<MenuDataModel> getMenu(){
+    public List<MenuDataModel> getMenu() {
         readMenu();//create a new menu for each table, on request
         return listMenu;
     }
