30d62c8740bcb4c6e9d28ce3b5f9bdcba5102f61
Terec Andrei <driver71528@gmail.com>
2023-03-28 12:59:05 +0300

    Added the 5th attribute and the test specifications generated from testlink

--- src/test/java/pizzashop/service/PizzaServiceTest.java
+++ src/test/java/pizzashop/service/PizzaServiceTest.java
@@ -1,26 +1,23 @@
 package pizzashop.service;
 
-import jdk.jfr.Unsigned;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import pizzashop.model.PaymentType;
 import pizzashop.repository.MenuRepository;
-import pizzashop.repository.PaymentRepository;
 import pizzashop.repository.PaymentRepositoryMock;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class PizzaServiceTest {
 
     PaymentRepositoryMock paymentRepositoryMock;
     PizzaService service;
+
     @BeforeEach
     void setUp() {
         paymentRepositoryMock = new PaymentRepositoryMock();
@@ -31,6 +28,7 @@ class PizzaServiceTest {
     @MethodSource("validPayments")
     @DisplayName("whenPaymentValidThenAddPayment")
     @Tag("PizzaService")
+    @Order(1)
     void addPayment_valid(int table, PaymentType type, double amount) {
         //Arrange test
         //This is done in the constructor
@@ -44,6 +42,7 @@ class PizzaServiceTest {
     @ParameterizedTest
     @MethodSource("invalidPayments")
     @Tag("PizzaService")
+    @Order(2)
     void addPayment_invalid(int table, PaymentType type, double amount, String expectedErrorMessage) {
         //Arrange test
         //This is done in the constructor
@@ -54,14 +53,9 @@ class PizzaServiceTest {
         Assertions.assertThrows(RuntimeException.class, test, expectedErrorMessage);
     }
 
-    @Test
-    void dummyTest() {
-    }
-
     private static Stream<Arguments> validPayments() {
         //int table, PaymentType type, double amount
-        return Stream.of(
-                arguments(1, PaymentType.Cash, 1), //ECP
+        return Stream.of(arguments(1, PaymentType.Cash, 1), //ECP
                 arguments(2, PaymentType.Cash, 10), // BVA la table=2
                 arguments(1000, PaymentType.Cash, 10) // BVA la table=1000
         );
@@ -69,10 +63,9 @@ class PizzaServiceTest {
 
     private static Stream<Arguments> invalidPayments() {
         //int table, PaymentType type, double amount, String expectedErrorMessage
-        return Stream.of(
-                arguments(0, PaymentType.Card, 2.5,"Invalid table"), //ECP
-                arguments(999, PaymentType.Cash, 0,"Invalid amount"), //ECP si BVA la table=999
-                arguments(1001, PaymentType.Card, 10.35,"Invalid table") //ECP si BVA la table=1001
+        return Stream.of(arguments(0, PaymentType.Card, 2.5, "Invalid table"), //ECP
+                arguments(999, PaymentType.Cash, 0, "Invalid amount"), //ECP si BVA la table=999
+                arguments(1001, PaymentType.Card, 10.35, "Invalid table") //ECP si BVA la table=1001
         );
     }
 }
